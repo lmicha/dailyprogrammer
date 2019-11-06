751aec77a7eb6728feebd6feefdcf56e00d27581
larsm <larsm@DESKTOP-JHUPCA6>
2019-11-06 13:37:33 +0100

    Creating Project

---
+++ hard/src/main/java/dailyprogrammer/hard/App.java
@@ -0,0 +1,13 @@
+package dailyprogrammer.hard;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ medium/src/main/java/dailyprogrammer/medium/App.java
@@ -0,0 +1,13 @@
+package dailyprogrammer.medium;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
